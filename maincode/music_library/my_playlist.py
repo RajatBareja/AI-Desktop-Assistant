a409a1855a81ccf168a51c949ca07570288db652
tulsirajrajat2210 <tulsirajrajat2210@gmail.com>
2026-02-20 18:04:18 +0530

    my library

---
+++ maincode/music_library/my_playlist.py
@@ -0,0 +1,5 @@
+music = {
+    "stealth":"https://www.youtube.com/watch?v=TrPvQvbp3Cg&list=RDTrPvQvbp3Cg&start_radio=1",
+    "march":"https://www.youtube.com/watch?v=1GWyCJHuNms&list=RDGMEM916WJxafRUGgOvd6dVJkeQ&start_radio=1&rv=TrPvQvbp3Cg",
+    "skyfall":"https://www.youtube.com/watch?v=rTuxUAuJRyY&list=RDGMEM916WJxafRUGgOvd6dVJkeQ&index=4"
+}
